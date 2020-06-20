7ec39f4c46c92711b155402c8bcf3e2349e3501a
Maduka Josh <Madukajosh@gmail.com>
2020-06-20 15:27:29 +0100

    Updates

---
+++ precache-manifest.0bad027e24d01079ec312dbd00b67031.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f32843318e066b17379f6fbef0251b1",
+    "url": "/mpractice/index.html"
+  },
+  {
+    "revision": "0a4b036ef39ba4c2a13a",
+    "url": "/mpractice/static/css/main.dcfc5f03.chunk.css"
+  },
+  {
+    "revision": "108190be9c313afe2c50",
+    "url": "/mpractice/static/js/2.f5b7c1d2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/mpractice/static/js/2.f5b7c1d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a4b036ef39ba4c2a13a",
+    "url": "/mpractice/static/js/main.b690edf1.chunk.js"
+  },
+  {
+    "revision": "3f1b88fdfef6b08eedd3",
+    "url": "/mpractice/static/js/runtime-main.c4c14780.js"
+  },
+  {
+    "revision": "c97cb84b74a52edcb496295687e87822",
+    "url": "/mpractice/static/media/cold-bg.c97cb84b.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/mpractice/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "635ed87d2cc826fb213057ac40dfaff1",
+    "url": "/mpractice/static/media/warm-bg.635ed87d.jpg"
+  }
+]);
